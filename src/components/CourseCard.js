fe5acbec0ce1568ed778a92fc62c42aae2668100
MayJh4307 <norit.hunhan@hotmail.com>
2020-12-29 00:34:45 +0700

    fini1

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -1,4 +1,27 @@
 export const CourseCard = (props) => {
   // TODO: design HTML component that displays a course as a "card" on the webpage.
-  return (null);
+  return <>
+  <table id="ta">
+    <tbody>
+    <tr>
+      <td id="fst">
+        <p>{props.name}</p>
+        <p>Unit : {props.crd}</p>
+        <p>Grade : {props.grd}</p>
+      </td>
+      <td id="but"><button onClick = {() =>props.del(props.name)} > delete </button></td>
+    </tr>
+    </tbody>
+  </table>
+  
+  </>;
 };
+/*
+
+  <div id="divf">
+      <p>{props.name}</p>
+      <p>Unit : {props.crd}</p>
+      <p>Grade : {props.grd}</p>
+      <button onClick = {() =>props.del(props.name)} > delete </button>
+  </div>
+  */
